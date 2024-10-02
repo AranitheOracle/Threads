b1a12fcbfced30380cec0a1a214e8e09832f0f1e
Arani Ghosh <147254190+AranitheOracle@users.noreply.github.com>
2024-10-03 00:03:19 +0530

    first commit

---
+++ frontend/src/main.jsx
@@ -0,0 +1,37 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {ChakraProvider} from '@chakra-ui/react'
+
+
+const styles = {
+	global: (props) => ({
+		body: {
+			color: mode("gray.800", "whiteAlpha.900")(props),
+			bg: mode("gray.100", "#101010")(props),
+		},
+	}),
+};
+
+const config = {
+	initialColorMode: "dark",
+	useSystemColorMode: true,
+};
+
+const colors = {
+	gray: {
+		light: "#616161",
+		dark: "#1e1e1e",
+	},
+};
+
+createRoot(document.getElementById('root')).render(
+
+  <React.StrictMode>
+    <ChakraProvider>
+
+    <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+)
